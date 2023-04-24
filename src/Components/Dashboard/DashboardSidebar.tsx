f3542d9101fc54d9111882a8762d12285158a7b7
nicsylvia <nicsylvia15f@gmail.com>
2023-04-23 19:07:22 -0700

    structured dash sidebar

--- src/Components/Dashboard/DashboardSidebar.tsx
+++ src/Components/Dashboard/DashboardSidebar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 const DashboardSidebar = () => {
-  return <div>DashboardSidebar</div>;
+  return (
+    <div>
+      <Container>DashboardSidebar</Container>
+    </div>
+  );
 };
 
 export default DashboardSidebar;
+
+const Container = styled.div`
+  width: 250px;
+  height: calc(100vh - 70px);
+  background-color: chocolate;
+`;
